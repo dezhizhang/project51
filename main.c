3179363f8cdf4dda743f972e2f576e6777b12ca4
dezhizhang <1541609448@qq.com>
2026-09-19 17:33:05 +0800

    feat: 单片机制控流水线

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 /*
- * :file description: 
+ * :file description:
  * :name: /project51/main.c
  * :author: 张德志
  * :date created: 2026-09-19 15:18:26
  * :last editor: 张德志
- * :date last edited: 2026-09-19 16:36:06
+ * :date last edited: 2026-09-19 17:31:43
  */
 /*
  * main.c — 51 单片机入门程序:LED 流水灯
@@ -17,37 +17,18 @@
  *           如果你的板子是高电平点亮,把 main 里的取反逻辑改一下即可
  */
 
-#include <8052.h>    /* SDCC 自带的 8052 内核寄存器定义(P0/P1/P2/P3...) */
+#include <8052.h> /* SDCC 自带的 8052 内核寄存器定义(P0/P1/P2/P3...) */
 #include <REGX52.H>
 #include <INTRINS_H>
-#define LED_PORT P1  /* LED 所在端口,板子不同可改成 P2 / P0 */
+#define LED_PORT P1 /* LED 所在端口,板子不同可改成 P2 / P0 */
 
 
-void Delay500ms() 
-{
-    unsigned char i,j, k;
-
-    _nop_();
-    i=4;
-    j=205;
-    k=187;
-
-    do{
-        do {
-            while(--k);
-
-        } while(--j);
-    } while(--i);
-
-}
-
-
-void main(void)
-{
-    while(1) {
-        P2=0xFE;
-        Delay500ms();
-        P2=0xFF;
-        Delay500ms();
+void main() {
+    while(1){
+      if(P3_1 == 0) {
+        P2_0=0;
+      }else {
+        P2_0=1;
+      }
     }
 }
